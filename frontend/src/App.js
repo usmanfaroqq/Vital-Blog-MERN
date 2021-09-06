38fd5160c2110264d80e20c86c6b68204485e638
Md. Osman Faruk <programmming.code@gmail.com>
2021-09-06 15:22:11 +0600

    sass added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import NotFound from './common/Notfound/NotFound';
+import Home from './components/Home/Home';
+import './scss/main.scss';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Router>
+    <Switch>
+      <Route path="/" exact component={Home}/>
+      <Route path="*" exact component={NotFound}/>
+    </Switch>
+  </Router>
   );
 };
 
---
+++ frontend/src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Helmet} from "react-helmet";
+
+const Home = () => {
+    return (
+        <>
+            <Helmet>
+                <title>Vital Blog</title>
+                <meta name="description" content="Learn HTML css js etc"/>
+            </Helmet>
+        </>
+    );
+};
+
+export default Home;
