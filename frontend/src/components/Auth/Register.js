3810696203f296218a13def84037b7d0d54d476c
Md. Osman Faruk <programmming.code@gmail.com>
2021-09-07 17:01:10 +0600

    registerError get

--- frontend/src/components/Auth/Register.js
+++ frontend/src/components/Auth/Register.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
-
+import { useDispatch } from "react-redux";
+import axios from "axios";
 const Register = () => {
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const handleInputs = (event) => {
+    setState({
+      ...state,
+      [event.target.name]: [event.target.value],
+    });
+  };
+  const dispatch = useDispatch();
+  const userRegister = async (event) => {
+    event.preventDefault();
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    dispatch({ type: "SET_LOADER" });
+    try {
+      const response = await axios.post("/register", state, config); // posting register data to server
+      console.log(response);
+      dispatch({ type: "CLOSE_LOADER" });
+    } catch (error) {
+      dispatch({ type: "CLOSE_LOADER" });
+      dispatch({type: 'REGISTER_ERRORS', payload: error.response.data.errors}); //errors
+      console.log(error.response);
+    }
+  };
   return (
     <>
       <Helmet>
@@ -15,16 +46,16 @@ const Register = () => {
           <Col md={12}>
             <div className="account">
               <div className="account__section">
-                <h1>
-                  Create your account
-                </h1>
-                <form>
+                <h1>Create your account</h1>
+                <form onSubmit={userRegister}>
                   <div className="group">
                     <input
                       type="text"
                       name="name"
                       className="group__control"
                       placeholder="Name"
+                      value={state.name}
+                      onChange={handleInputs}
                     />
                   </div>
                   <div className="group">
@@ -41,6 +72,8 @@ const Register = () => {
                       name="email"
                       className="group__control"
                       placeholder="Email"
+                      value={state.email}
+                      onChange={handleInputs}
                     />
                   </div>
                   <div className="group">
@@ -49,6 +82,8 @@ const Register = () => {
                       name="password"
                       className="group__control"
                       placeholder="Password"
+                      value={state.password}
+                      onChange={handleInputs}
                     />
                   </div>
                   <div className="group">
@@ -59,7 +94,9 @@ const Register = () => {
                     />
                   </div>
                 </form>
-                <h4>Have an account <Link to="/login">Login here</Link></h4>
+                <h4>
+                  Have an account <Link to="/login">Login here</Link>
+                </h4>
               </div>
             </div>
           </Col>
--- frontend/src/redux/reducers/AuthReducer.js
+++ frontend/src/redux/reducers/AuthReducer.js
@@ -1,12 +1,18 @@
 const initState = {
-    loading: false,
-    loginErrors: [],
-    registerErrors: [],
-}
-
+  loading: false,
+  loginErrors: [],
+  registerErrors: [],
+};
 
 const AuthReducer = (state = initState, action) => {
-    return state;
+  if (action.type === "SET_LOADER") { 
+    return { ...state, loading: true }; 
+  } else if (action.type === "CLOSE_LOADER") {
+    return { ...state, loading: false };
+  } else if (action.type === "REGISTER_ERRORS"){
+      return {...state, registerErrors: action.payload}; // getting errors
+  }
+  return state;
 };
 
 export default AuthReducer;
