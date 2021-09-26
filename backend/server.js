22e47c4c994c173478e2b57a9649bf29844f59fb
Md. Osman Faruk <programmming.code@gmail.com>
2021-09-26 16:37:07 +0600

    solvingBug

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,10 @@ dbConnect();
 app.use(bodyParser.json());
 app.use(cors())
 
-// Routes
-app.get("/", (req, res) => {
-  res.send("Welcome to vital Blog");
-}); // Home Route
+// // Routes
+// app.get("/", (req, res) => {
+//   res.send("Welcome to vital Blog");
+// }); // Home Route
 
 app.use('/', authRouter) // Register/login Function
 app.use('/', postRouter) // post/get Route
@@ -35,7 +35,7 @@ app.use('/', profileRouter) // post/get Route
 // Server configuration
 const PORT = process.env.PORT || 4000;
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build/')));
+  app.use(express.static(path.join(__dirname, `./../frontend/build/`)));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   })
